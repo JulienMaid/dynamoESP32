db6cdf359b697b646e911992b0231abd7d6e02b4
Julien B. <jbonne79@yahoo.fr>
2025-06-10 21:44:20 +0200

    Ajout appel vers controleur LEDs WS2811

--- dynamoESP32.ino
+++ dynamoESP32.ino
@@ -5,12 +5,15 @@
 #include "RTC_soft.h"
 #include "trace_debug.h"
 #include "convertAnalogValue.h"
+#include "esp32WS2811.h"
 
 Ticker g_t_blinker;
 TimerEvent_t TimerTOP;
+
 ConvertAnalogValue ConvertVoltage(0, 0, 24.0, 0.0, 0, 1024);
 ConvertAnalogValue Convertcurrent(512, 10, 10.0, -10.0, 0, 1024);
 
+WS2811 bandeauLED(2,30);
 
 void setup()
 {
